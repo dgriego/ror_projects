0110f3144c7f70a11f5faa6e85f6738081275c84
dgriego <daniel@jackrabbitsystems.com>
2017-03-13 15:17:10 -0600

    adding group users and associations

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
+  has_many :group_users
+  has_many :groups, :through => :group_users
 end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312041059) do
+ActiveRecord::Schema.define(version: 20170313205058) do
+
+  create_table "group_users", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
